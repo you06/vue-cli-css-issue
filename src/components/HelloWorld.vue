acf298d2c3b191f4dfb5c4994cde839787e2061b
you06 <you1474600@gmail.com>
2018-03-03 12:12:43 +0800

    Initial

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="hello">
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HelloWorld',
+  props: {
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+.hello {
+  width: 1000px;
+  height: 500px;
+  background-image: linear-gradient(top, #ffffff, #2196F3);
+}
+</style>
